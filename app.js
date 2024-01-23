5b9a84301b25edca9f6855075fbfe827630e622e
carlalaya <carla.laya@gmail.com>
2024-01-23 11:06:49 -0300

    Inicio repositorio

---
+++ app.js
@@ -0,0 +1,59 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+
+
+//validar usuario ingresado en el input
+function verificarIntento () {
+    //colocar parseInt para convertir el valor en un numero
+    let numerodeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(intentos);
+    
+    if (numerodeUsuario === numeroSecreto) {
+        asignarTextoElemento('p' , `¡Te felicito! Acertaste el numero en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'} `);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numerodeUsuario > numeroSecreto){
+            asignarTextoElemento('p' , 'El numero secreto es menor');
+        } else{
+            asignarTextoElemento('p' , 'El numero secreto es mayor');
+        }
+        intentos ++;
+        limpiarCampo();
+    }
+    return;
+}
+
+function limpiarCampo(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+// agrego paramentros
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return; 
+}
+
+//funcion para generar un numero aleatorio
+function generarNumeroSecreto (){
+    let numeroSecreto = Math.floor(Math.random()*10)+1;
+    return numeroSecreto;
+
+}
+console.log(numeroSecreto);
+
+function condicionesIniciales () {
+    asignarTextoElemento('h1' ,'Adivina el nùmero secreto');
+    asignarTextoElemento('p' , 'Ingrese un nùmero del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego () {
+    limpiarCampo();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled' , 'true');
+}
+
+condicionesIniciales()
+
